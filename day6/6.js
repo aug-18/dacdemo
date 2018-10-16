8482d1c42642a62fe306db4a9ed44c1e7d72283f
santosh <santosh.ece06@gmail.com>
2018-10-16 12:32:00 +0530

    day 6

---
+++ day6/6.js
@@ -0,0 +1,33 @@
+
+function postOnYourWall(){
+    try{
+        console.log("POST LOGIG WILL BE DONE HERE...");
+
+        // READ THE INPUT
+        let inputRef = document.getElementById("inputId");
+        let postValue = inputRef.value; // ITS PROPERTY NOT METHOD.
+
+        // CLEAR THE INPUT BOX
+        inputRef.value = "";
+
+        // 3. GET THE REF BOX.
+        let refBoxBlock = document.getElementById("refBoxId");
+
+        // 4. Create clone
+        let newBlock = refBoxBlock.cloneNode(true);
+        newBlock.style.visibility = "visible";
+
+        // 5 
+        newBlock.removeAttribute("id");
+
+        // 6
+        newBlock.children[1].innerHTML = postValue;
+
+        // 7 
+        let parentRef = document.getElementById("parentId");
+        parentRef.insertBefore(newBlock, parentRef.firstChild);
+
+    }catch(err){
+        console.log(err);
+    }
+}
